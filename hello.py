b84adcd505beae77628348a4af96e2d56eea4028
bxljeet <166347995+bxljeet@users.noreply.github.com>
2024-04-19 16:14:37 +1200

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,12 @@
+# python progras to check if year is a leap year or not
+
+year int(input (enter a Year:\n))
+
+if (year % 400 - 0) and (year x 100 -= e):
+print (year, "is a leap year*)
+       
+elif (year % 4 --B) and Cyear & 100 (= 0): 
+print (year, "is a leap year*)
+       
+else:
+print (year is not a leap year 2
